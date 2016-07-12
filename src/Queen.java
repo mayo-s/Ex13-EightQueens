864f89c65d30756ce73b8e31bb1b1c52cc126c34
Eli Gould <MariaKatharinaWunder@gmail.com>
2016-07-12 10:48:11 +0200

    Make queen class with basic methods

---
+++ src/Queen.java
@@ -0,0 +1,28 @@
+
+public class Queen {
+
+	private int xCord;
+	private int yCord;
+	
+	public Queen(int xCord, int yCord){
+		this.xCord = xCord;
+		this.yCord = yCord;
+		
+	}
+	
+	public void setxCord(int xCord){
+		this.xCord = xCord;
+	}
+	
+	public void setyCord(int yCord){
+		this.xCord = yCord;
+	}
+	
+	public int getyCord(){
+		return yCord;
+	}
+	
+	public int getxCord(){
+		return xCord;
+	}
+}
